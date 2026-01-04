b46c3d96688e8fed1d312ec3b3bf7084b35023e4
musicadarlan1-design <musicadarlan1@gmail.com>
2026-01-04 19:35:32 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,4 @@
-// INFINITY CLOUD SERVICE WORKER V75.4
-const CACHE_NAME = "infinity-v75-4-fix";
+const CACHE_NAME = "infinity-v75-4-img"; // Atualizado para forçar o navegador a baixar o novo CSS
 const ASSETS = [
     "./",
     "./index.html",
@@ -10,42 +9,20 @@ const ASSETS = [
     "https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.1.1/crypto-js.min.js"
 ];
 
-// Instalação: Salva arquivos no cache
 self.addEventListener("install", (e) => {
     self.skipWaiting();
-    e.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => {
-            return cache.addAll(ASSETS);
-        })
-    );
+    e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(ASSETS)));
 });
 
-// Ativação: Limpa caches de versões anteriores
 self.addEventListener("activate", (e) => {
-    e.waitUntil(
-        caches.keys().then((keys) => {
-            return Promise.all(
-                keys.map((key) => {
-                    if (key !== CACHE_NAME) {
-                        return caches.delete(key);
-                    }
-                })
-            );
-        }).then(() => self.clients.claim())
-    );
+    e.waitUntil(caches.keys().then((k) => Promise.all(k.map((i) => i !== CACHE_NAME ? caches.delete(i) : null))).then(() => self.clients.claim()));
 });
 
-// Fetch: Tenta buscar do cache, se não tiver vai na rede
 self.addEventListener("fetch", (e) => {
-    // IMPORTANTE: Deixa as chamadas do Telegram passarem direto (não salvar em cache)
     if (e.request.url.includes("api.telegram.org")) {
         e.respondWith(fetch(e.request));
         return;
     }
-
-    e.respondWith(
-        caches.match(e.request).then((res) => {
-            return res || fetch(e.request);
-        })
-    );
+    e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
 });
+
