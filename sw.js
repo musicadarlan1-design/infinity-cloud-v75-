124c83e25ecb09d63d5666d5ad9529f04a77af87
musicadarlan1-design <musicadarlan1@gmail.com>
2026-01-05 12:39:48 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,9 @@
-const CACHE_NAME = "infinity-v75-3";
+const CACHE_NAME = "infinity-v75-3-fix";
 const ASSETS = ["./", "./index.html", "./style.css", "./script.js", "./manifest.json"];
 self.addEventListener("install", (e) => {
-    self.skipWaiting();
     e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(ASSETS)));
-});
-self.addEventListener("activate", (e) => {
-    e.waitUntil(caches.keys().then((ks) => Promise.all(ks.map((k) => k !== CACHE_NAME ? caches.delete(k) : null))));
 });
 self.addEventListener("fetch", (e) => {
     if (e.request.url.includes("api.telegram.org")) return;
-    e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request)));
+    e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
 });
