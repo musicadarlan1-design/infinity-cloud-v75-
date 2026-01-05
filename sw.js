ab00682101a6d03f6e893b5a4a4374f506d6ccf7
musicadarlan1-design <musicadarlan1@gmail.com>
2026-01-04 23:22:40 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,8 @@
-const CACHE_NAME = "infinity-ultra-v75-5-3";
-
-const ASSETS = [
-    "./",
-    "./index.html",
-    "./style.css",
-    "./script.js",
-    "./manifest.json",
-    "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css",
-    "https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.1.1/crypto-js.min.js",
-    "https://cdnjs.cloudflare.com/ajax/libs/lz-string/1.5.0/lz-string.min.js"
-];
+const CACHE_NAME = "infinity-v75-5-4";
+const ASSETS = ["./", "./index.html", "./style.css", "./script.js", "./manifest.json", 
+"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css",
+"https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.1.1/crypto-js.min.js",
+"https://cdnjs.cloudflare.com/ajax/libs/lz-string/1.5.0/lz-string.min.js"];
 
 self.addEventListener("install", (e) => {
     self.skipWaiting();
@@ -17,19 +10,10 @@ self.addEventListener("install", (e) => {
 });
 
 self.addEventListener("activate", (e) => {
-    e.waitUntil(
-        caches.keys().then((keys) => {
-            return Promise.all(
-                keys.map((key) => {
-                    if (key !== CACHE_NAME) return caches.delete(key);
-                })
-            );
-        }).then(() => self.clients.claim())
-    );
+    e.waitUntil(caches.keys().then((ks) => Promise.all(ks.map((k) => k !== CACHE_NAME ? caches.delete(k) : null))));
 });
 
 self.addEventListener("fetch", (e) => {
     if (e.request.url.includes("api.telegram.org")) return;
-    e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+    e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request)));
 });
-
